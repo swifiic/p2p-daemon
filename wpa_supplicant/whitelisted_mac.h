778b4ca9da9e613e12575bf48b0a4440c8248642
arnav dhamija <arnav.dhamija@gmail.com>
2018-02-03 10:54:54 +0530

    updated wl

--- wpa_supplicant/whitelisted_mac.h
+++ wpa_supplicant/whitelisted_mac.h
@@ -1,6 +1,7 @@
 char whitelist_macs[][30] = {
     "ac:2b:6e:70:94:42",
-    "44:85:00:15:ff:a0"
+    "44:85:00:15:ff:a0",
+    "ba:27:eb:c2:fa:57"
 };
 
-int whitelisted_count = 2;
+int whitelisted_count = 3;
