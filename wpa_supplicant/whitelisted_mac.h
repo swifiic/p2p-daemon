b999735258fb25497f5ec2d37f49bc335d00aefc
arnav dhamija <arnav.dhamija@gmail.com>
2018-02-04 14:48:24 +0530

    whitelist

--- wpa_supplicant/whitelisted_mac.h
+++ wpa_supplicant/whitelisted_mac.h
@@ -1,8 +1,8 @@
 char whitelist_macs[][30] = {
-    "54:27:1e:c8:2b:37",
-    "ac:2b:6e:70:94:42",
-    "44:85:00:15:ff:a0",
-    "ba:27:eb:c2:fa:57"
+    "54:27:1e:c8:2b:37", //asus netbook
+    "ac:2b:6e:70:94:42", //infocus
+    "44:85:00:15:ff:a0", //thinkpad
+    "ba:27:eb:c2:fa:57" //rpi3
 };
 
-int whitelisted_count = 3;
+int whitelisted_count = 4;
